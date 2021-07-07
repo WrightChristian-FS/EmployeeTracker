8e4cb87f17bd27e8791d6eaa7f15725482334e27
WrightChristian-FS <cmwright@student.fullsail.edu>
2021-07-06 20:29:17 -0700

    Created Employee Tracker solution

---
+++ EmployeeTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
